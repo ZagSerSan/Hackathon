755713b8687a253687a0b7eb3208a493559a9abe
ZagSerSan <yan.steys1997@gmail.com>
2023-03-24 22:43:28 +0100

    creating a context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import './styles.css'
 
+import {ContextMenu} from './menu'
+ 
+const contextMenu = new ContextMenu();
+contextMenu.open();
+contextMenu.close();
+
+const menuItems = document.querySelectorAll('.menu-item');
+menuItems.forEach(item => {
+  item.addEventListener('click', event => {
+    console.log(item.textContent);
+  })
+})
