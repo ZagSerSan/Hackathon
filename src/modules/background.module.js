2730e95d8ecc412837f428165dc76d0060de5446
ZagSerSan <yan.steys1997@gmail.com>
2023-03-26 19:06:15 +0200

    upd bg module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -6,8 +6,15 @@ export class BackgroundModule extends Module {
    }
    trigger() {
       // функционал модуля
-      console.log('Случайный фон сработал!');
-      document.body.style.backgroundColor = `#${Math.random().toString(16).substring(2,8)}`;
+      // document.body.style.backgroundColor = `#${Math.random().toString(16).substring(2,8)}`;
+      function getRandomInt(max) {
+         return Math.floor(Math.random() * max);
+      }
+      // количество изображений
+      const imageCount = 4
+      document.body.style.backgroundImage = `url(./src/assets/bgimg/bg_${getRandomInt(imageCount)}.webp)`
+      document.body.style.backgroundPosition = 'center'
+      document.body.style.backgroundSize = 'cover'
    }
    toHTML() {
       return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
